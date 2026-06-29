3007f8f72b490f7de45996d37b81a8d3d7ad006b
elena_dobruk <elena.dobruk@yandex.ru>
2026-06-29 18:22:07 +0300

    add brain-calc game with a new game engine

---
+++ games/calc.js
@@ -0,0 +1,47 @@
+const MIN = 1
+const MAX = 99
+const MESSAGE = 'What is the result of the expression?'
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min)
+  const maxFloored = Math.floor(max)
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+}
+
+function getRandomSign() {
+  const result = Math.floor(Math.random() * 3)
+
+  switch (result) {
+    case 0:
+      return '+'
+    case 1:
+      return '-'
+    default:
+      return '*'
+  }
+}
+
+function makeCalc() {
+  const num1 = getRandomInt(MIN, MAX)
+  const num2 = getRandomInt(MIN, MAX)
+
+  const operator = getRandomSign()
+  const question = `${num1} ${operator} ${num2}`
+  let correctAnswer
+
+  switch (operator) {
+    case '+':
+      correctAnswer = num1 + num2
+      break
+    case '-':
+      correctAnswer = num1 - num2
+      break
+    default:
+      correctAnswer = num1 * num2
+      break
+  }
+
+  return [question, correctAnswer]
+}
+
+export { makeCalc, MESSAGE }
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync'
+
+const NUMBER_OF_ROUNDS = 3
+
+function play(game, message) {
+  console.log('Welcome to the Brain Games!')
+  const userName = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${userName}!`)
+  console.log(message)
+
+  let gameResult = 'win'
+
+  for (let i = 0; i < NUMBER_OF_ROUNDS; i += 1) {
+    const [question, correctAnswer] = game()
+    console.log(`Question: ${question}`)
+
+    const userAnswer = parseInt(readlineSync.question('Your answer: '))
+
+    if (correctAnswer === userAnswer) {
+      console.log('Correct!')
+    }
+    else {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`)
+      console.log(`Let's try again, ${userName}!`)
+      gameResult = 'lose'
+      break
+    }
+  }
+
+  if (gameResult === 'win') {
+    console.log(`Congratulations, ${userName}!`)
+  }
+}
+
+export default play
